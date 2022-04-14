46d3e0e52176464a9f5a0fe223edecddc564531f
keleris32 <charles.ejiegbu@gmail.com>
2022-04-14 16:42:46 +0100

    feat: hide splash screen after interval

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { ActivityIndicator } from 'react-native';
 
 import AuthNavigator from './AuthNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const AppNavContainer = () => {
+  const [isAuthLoaded, setIsAuthLoaded] = useState<boolean>(false);
+
+  const handleAuthLoaded = () => setIsAuthLoaded(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      handleAuthLoaded();
+    }, 5000);
+  }, []);
+
+  useEffect(() => {
+    if (isAuthLoaded) {
+      SplashScreen.hide();
+    }
+  }, [isAuthLoaded]);
+
   return (
     <>
-      <NavigationContainer>
-        <AuthNavigator />
-      </NavigationContainer>
+      {isAuthLoaded ? (
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+      ) : (
+        <ActivityIndicator />
+      )}
     </>
   );
 };
